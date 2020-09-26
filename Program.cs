c7a2d855f4aa7dec4e69862b2da42d4f999e0080
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-09-25 22:32:05 -0300

    Resolução do Exercício

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int numero1;
+            int numero2;
+
+            Console.Write("A soma de: ");
+            numero1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Mais..: ");
+            numero2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine($"é igual a: {numero1 + numero2}");
+            Console.WriteLine();
+            
+            Console.WriteLine("Digite qualquer tecla para finalizar...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
